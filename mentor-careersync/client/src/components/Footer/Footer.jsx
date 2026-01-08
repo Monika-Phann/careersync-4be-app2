f8212cd15f31e2501ab21df4a43c093c0e3d2b0c
hour <Songhour10dl@gmail.com>
2026-01-09 00:29:22 +0700

    cahnegd footer

--- mentor-careersync/client/src/components/Footer/Footer.jsx
+++ mentor-careersync/client/src/components/Footer/Footer.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
-import { FooterStyles } from './Footer.styles'
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { FooterStyles } from "./Footer.styles";
 
 function Footer() {
   return (
     <Box component="footer" sx={FooterStyles.footer}>
       <Typography variant="body2" sx={FooterStyles.text}>
-        © 2025 CAREERSONC. All rights reserved.
+        © 2025 CAREERSSYNC. All rights reserved.
       </Typography>
     </Box>
-  )
+  );
 }
 
-export default Footer
-
+export default Footer;
